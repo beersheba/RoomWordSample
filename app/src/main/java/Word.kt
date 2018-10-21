e50fdd48c63709b821c2e7de1ae6b227dac97f9f
Andrey Rankov <andreyr@appcard.com>
2018-10-21 22:20:53 +0300

    Create the entity

---
+++ app/src/main/java/Word.kt
@@ -0,0 +1,7 @@
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String) {
+}
